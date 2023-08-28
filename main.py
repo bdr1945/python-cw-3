e44c90fdd60101364c847e61da77bf176e172a18
bdr1945 <142620983+bdr1945@users.noreply.github.com>
2023-08-28 19:08:28 +0300

    dd

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 # write your code here
+fav_animals = {"dog", "cat", "monkey", "rabbit"}
+print(fav_animals)
+print(fav_animals[3])
+fav_animals.remove("monkey")
+fav_animals.append("cow")
+fav_animals.append("monkey")
+for animal in fav_animals:
+    print(f"i love {animal}")
+numbers = [5,4,3,2,1]
+numbers_sum = 0
+for num in numbers:
+    numbers_sum.append(num + numbers_sum)   
